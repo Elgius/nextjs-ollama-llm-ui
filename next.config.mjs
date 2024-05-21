00124d61928cdfb339c6fee2f4ceb11af0f92512
Elgius <114131653+Elgius@users.noreply.github.com>
2024-05-21 13:02:18 +0500

    tester code

--- next.config.mjs
+++ next.config.mjs
@@ -15,11 +15,32 @@ const nextConfig = {
     return config;
   },
   typescript: {
-    // !! WARN !!
+    //!! WARN!!
     // Dangerously allow production builds to successfully complete even if
     // your project has type errors.
-    // !! WARN !!
+    //!! WARN!!
     ignoreBuildErrors: true,
+  },
+  async headers() {
+    return [
+      {
+        // Apply CORS headers to all API routes
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" }, // Adjust this to your actual origin or use "*"
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,DELETE,PATCH,POST,PUT",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+          },
+        ],
+      },
+    ];
   },
 };
 
